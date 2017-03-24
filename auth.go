dc6e239e92561aeac67f79fa092b436d36f2b1d8
Fabian Wenzelmann <fabianwenzelmann@posteo.de>
2017-03-24 17:42:14 +0100

    documentation

--- auth.go
+++ auth.go
@@ -44,17 +44,23 @@ import (
 // "Basic" types such as int, string, ... work fine.
 type UserKeyType interface{}
 
+// ErrKeyNotFound is the error that is returned whenever you try to lookup
+// the information stored for a certain key but that key does not exist.
 var ErrKeyNotFound = errors.New("No entry for key was found")
 
 const (
-	// The default length for random bytes array, this creates random base64
-	// strings of length 64.
+	// DefaultRandomByteLength is the default length for random bytes array, this
+	// creates random base64 strings of length 64.
 	DefaultRandomByteLength = 48
-	DefaultKeyLength        = 64
+
+	// DefaultKeyLength is the length of the random base 64 strings, it should
+	// be set in accordance with DefaultRandomByteLength.
+	DefaultKeyLength = 64
 )
 
 // SessionKeyData type is used as a result in a key lookup. It contains the user
-// that corresponded to the session key and the time it was created.
+// that corresponds to the session key and the time it was created and the time
+// when the key becomes invalid.
 // All methods that accept a *SessionKeyData should assume that the lookup
 // failed if it is nil.
 // The time should *always* be in UTC so the behaviour is consistent (and UTC
@@ -62,11 +68,15 @@ const (
 // A key is considered valid if currentTime <= ValidUntil. You can use
 // the helper functions KeyValid(now, ValidUntil) or KeyInvalid(now, ValidUntil),
 // or directly use these constraints directly in your database queries.
-// The helper function take care that the <= constraint is used correctly.
 type SessionKeyData struct {
-	User         UserKeyType
+	// User is the user connected with a key.
+	User UserKeyType
+
+	// CreationTime is the time the key was created.
 	CreationTime time.Time
-	ValidUntil   time.Time
+
+	// ValidUntil is the time until the key is considered valid.
+	ValidUntil time.Time
 }
 
 // NewSessionKeyData creates a new SessionKeyData instance with the given
@@ -77,22 +87,11 @@ func NewSessionKeyData(user UserKeyType, creationTime, validUntil time.Time) *Se
 	return &SessionKeyData{User: user, CreationTime: creationTime, ValidUntil: validUntil}
 }
 
-// This method returns the current type. For consistent behaviour you should
+// CurrentTime returns the current type. For consistent behaviour you should
 // always use this method to get the current time.
 // It returns time.Now().UTC()
 func CurrentTime() time.Time {
 	return time.Now().UTC()
-}
-
-// A key is considered invalid if now is after validUntil.
-// The parameter now exists s.t. you can use the same now in all queries, so
-// usually you create now once at the beginning of your function.
-func KeyInvalid(now, validUntil time.Time) bool {
-	return now.After(validUntil)
-}
-
-func KeyValid(now, validUntil time.Time) bool {
-	return !KeyInvalid(now, validUntil)
 }
 
 // CurrentTimeKeyData creates a new SessionKeyData object with the current time.
@@ -104,9 +103,25 @@ func CurrentTimeKeyData(user UserKeyType, validDuration time.Duration) *SessionK
 	return NewSessionKeyData(user, now, validUntil)
 }
 
-// Returns a random base64 encoded string based on a random byte sequence of
-// size n. Note that the returned string does not have length n, n is the size
-// of the byte array!
+// KeyInvalid checks if a key is invalid.
+// A key is considered invalid if now is after validUntil.
+// The parameter now exists s.t. you can use the same now in all queries, so
+// usually you create now once at the beginning of your function.
+func KeyInvalid(now, validUntil time.Time) bool {
+	return now.After(validUntil)
+}
+
+// KeyValid checks if a key is still valid.
+// This is the case if validUntil <= now.
+// The parameter now exists s.t. you can use the same now in all queries, so
+// usually you create now once at the beginning of your function.
+func KeyValid(now, validUntil time.Time) bool {
+	return !KeyInvalid(now, validUntil)
+}
+
+// GenRandomBase64 returns a random base64 encoded string based on a random byte
+// sequence of size n. Note that the returned string does not have length n,
+// n is the size of the byte array!
 // To represent n bytes in base64 you need 4*(n/3) rounded up to the next
 // multiple of 4. So this will be the length of the returned string.
 // For example use n = 24 for keys of length 32, n = 48 for keys of length 64
@@ -124,10 +139,10 @@ func GenRandomBase64(n int) (string, error) {
 	return base64.URLEncoding.EncodeToString(b), nil
 }
 
-// This i
-// TODO: int64 overflow
+// SessionHandler is the interface to store and retrieve session keys and
+// the associated SessionKeyData objects.
 type SessionHandler interface {
-	// Initialize storage s.t. it is ready for use. This could be for
+	// Init initializes the storage s.t. it is ready for use. This could be for
 	// example a create table statement. You should however not overwrite
 	// any existing data (if you have any).
 	// For example only create a table if it does not already exist.
@@ -135,7 +150,7 @@ type SessionHandler interface {
 	Init() error
 
 	// GetData is a function to get the user data for a given key.
-	// It should return nil as key and KeyNotFoundErr if the key was not found
+	// It should return nil and KeyNotFoundErr if the key was not found
 	// and nil and some other error in case something went wrong during lookup.
 	// If an err != nil is returned it must always return *SessionKeyData != nil.
 	GetData(key string) (*SessionKeyData, error)
@@ -162,12 +177,11 @@ type SessionHandler interface {
 	DeleteKey(key string) error
 }
 
-// This struct uses a SessionHandler to query the storage and add additional
-// functionality. It is used as the main anchorpoint for user authentication.
-// It uses a certain user key (string) to store authentication information
-// in a gorialla session store (creates a new session named ).
-// You should make sure this key is used nowehere else in your program for
-// sessions.
+// SessionController uses a SessionHandler to query the storage and add
+// additional functionality. It is used as the main anchorpoint for user
+// authentication.
+// It creates sessions called SessionName (the field in this struct) and stores
+// in that session the key in session.Values["key"].
 // NumBytes is the length of the random byte slice, see GenRandomBase64
 // for details about this parameter.
 type SessionController struct {
@@ -176,17 +190,20 @@ type SessionController struct {
 	SessionName string
 }
 
-// Create a new session controller given a SessionHandler, the size of the
-// random byte slice and the user key name.
-// Use numBytes = -1 for random keys of length 64.
-// If userKey is the empty string it defaults to "user-auth".
+// NewSessionController creates a new session controller given a SessionHandler,
+// the size of the random byte slice
+// If you use another key length or session name set the values after calling
+// NewSessionController, i.e. controller.NumBytes = ... and
+// controller.SessionName = ...
 func NewSessionController(h SessionHandler) *SessionController {
 	return &SessionController{SessionHandler: h, NumBytes: DefaultRandomByteLength,
 		SessionName: "user-auth"}
 }
 
 // AddKey adds a new entry to the storage.
-// This function returns either "" and an error or an created key and nil.
+// This function returns either nil, "" and some error if something went wrong
+// or the SessionKeyData instance, the key that was used to identify this
+// session and nil.
 func (c *SessionController) AddKey(user UserKeyType, validDuration time.Duration) (*SessionKeyData, string, error) {
 	key, genErr := GenRandomBase64(c.NumBytes)
 	if genErr != nil {
@@ -200,13 +217,20 @@ func (c *SessionController) AddKey(user UserKeyType, validDuration time.Duration
 	return data, key, nil
 }
 
-// gorialla session management
-
 const (
+	// SessionKey is the key to store the auth-key in a gorilla session.
+	// So we store the create create for the auth session in
+	// session.Values[SessionKey].
 	SessionKey = "key"
 )
 
+// ErrInvalidKey is the error that will be returned if a key was found in the
+// storage but the key is not valid anymore.
 var ErrInvalidKey = errors.New("The key is not valid any more.")
+
+// ErrNotAuthSession is the error that will be returned if a gorialla session
+// does not have the SessionKey in session.Values. This usually means that
+// the user does not have a session yet and needs to login.
 var ErrNotAuthSession = errors.New("The session is not a valid auth session.")
 
 // ValidateSession validates the key that is stored in the session.
@@ -220,24 +244,31 @@ var ErrNotAuthSession = errors.New("The session is not a valid auth session.")
 // the key in the underlying storage. It then returns nil, the session and
 // KeyNotFoundErr if the key was not found in the storage (for example the key
 // was deleted because it was not valid any more).
-// Otherwise it returns any error that may have happend while asking the
-// underlying storage, such as database errors.
 // If the key is still present in the storage but not valid any more
 // InvalidKeyErr will be returned. Otherwise it returns the data found in the
 // storage, the auth session object (for possible further processing) and nil
 // as error.
+// Otherwise it returns any error that may have happend while asking the
+// underlying storage, such as database errors.
 // So summarize:
-//   If it returns a *SessionKeyData != nil everything is ok, you can get the
-//   user information from the SessionKeyData element.
 //
-//   If it returns nil as for the SessionKeyData something went wrong:
-//   (1) Something was wrong with the store (2) err == NotAuthSessionErr no
-//   authentication information was found, so probably the user has to log in
-//   and create a new sessionn (3) err == KeyNotFoundErr auth information was
-//   provided, but the key was not found, so either someone tried a random key
-//   or the session of the user simply expired and was therefore deleted from
-//   storage (4) err == InvalidKeyErr the key was still found in the database
-//   but is not valid any more, so probably the user hast to login again.
+// If it returns a *SessionKeyData != nil everything is ok, you can get the
+// user information from the SessionKeyData element.
+//
+// If it returns nil as for the SessionKeyData something went wrong:
+// (1) Something was wrong with the store (2) err == NotAuthSessionErr no
+// authentication information was found, so probably the user has to log in
+// and create a new sessionn (3) err == KeyNotFoundErr auth information was
+// provided, but the key was not found, so either someone tried a random key
+// or the session of the user simply expired and was therefore deleted from
+// storage (4) err == InvalidKeyErr the key was still found in the database
+// but is not valid any more, so probably the user hast to login again.
+//
+// This method will automatically update the session.MaxAge to the time
+// the key is still considered valid. If the key is invalid it will set the
+// MaxAge to -1.
+//
+// This method will not call session.Save!
 //
 // See examples for how to use this method.
 func (c *SessionController) ValidateSession(r *http.Request, store sessions.Store) (*SessionKeyData, *sessions.Session, error) {
@@ -257,7 +288,7 @@ func (c *SessionController) ValidateSession(r *http.Request, store sessions.Stor
 
 	key, ok := keyVal.(string)
 	if !ok {
-		return nil, session, errors.New("Internal lookup error.")
+		return nil, session, errors.New("Internal lookup error. \"key\" is present in the session but not of type string.")
 	}
 
 	// try to get the information out of the underlying storage
@@ -270,18 +301,31 @@ func (c *SessionController) ValidateSession(r *http.Request, store sessions.Stor
 	// so we validate the entry and update the max age of the session, update to
 	// the time that is still left
 
-	durationLeft := info.ValidUntil.Sub(now)
-	session.Options.MaxAge = int(durationLeft / time.Second)
-
 	if KeyInvalid(now, info.ValidUntil) {
+		session.Options.MaxAge = -1
 		return nil, session, ErrInvalidKey
 	}
+
+	durationLeft := info.ValidUntil.Sub(now)
+	session.Options.MaxAge = int(durationLeft / time.Second)
 
 	// everything is fine, so now return everything: the user should be considered
 	// as logged in
 	return info, session, nil
 }
 
+// CreateAuthSession will create a new session and add it to the underlying
+// storage.
+// It returns the data that was stored for the key, the generated key
+// the goriall session the value was stored in and any error.
+// If err != nil you should always consider it as a failure and assume that
+// something went wrong on your server (internal server error).
+// It will return the session even if err != nil and we didn't store the key,
+// but that is not really important since you should always handle it as an
+// error.
+//
+// It will set the session.MaxAge to the correct value, but again will not
+// call session.Save!
 func (c *SessionController) CreateAuthSession(r *http.Request, store sessions.Store,
 	user UserKeyType, validDuration time.Duration) (*SessionKeyData, string, *sessions.Session, error) {
 	session, err := store.Get(r, c.SessionName)
@@ -298,8 +342,14 @@ func (c *SessionController) CreateAuthSession(r *http.Request, store sessions.St
 	return data, key, session, nil
 }
 
-// NotAuthSessionErr ==> not so bad
-// Something different ==> internal error
+// EndSession deletes the key stored in session.Values from the underlying
+// storage.
+// If the session does not contain an auth key it will not return an,
+// i.e. if the session is not an auth session we can't look up the key.
+// It will then return nil as an error. So an error is only returned if
+// something really went wrong.
+//
+// The session.MaxAge will be set to -1.
 func (c *SessionController) EndSession(r *http.Request, store sessions.Store) error {
 	session, err := store.Get(r, c.SessionName)
 	if err != nil {
@@ -310,18 +360,31 @@ func (c *SessionController) EndSession(r *http.Request, store sessions.Store) er
 	keyVal, hasKey := session.Values[SessionKey]
 
 	if !hasKey {
-		return ErrNotAuthSession
+		return nil
 	}
 
 	key, ok := keyVal.(string)
 	if !ok {
-		return errors.New("Internal lookup error.")
+		return errors.New("Internal lookup error. \"key\" is present in the session but not of type string.")
 	}
 	// set the session age to -1
 	session.Options.MaxAge = -1
 	return c.DeleteKey(key)
 }
 
+// DeleteEntriesDaemon starts a goroutine that runs forever and deletes invalid
+// keys from the underlying storage.
+//
+// The sleep parameter specifies how often entries should be deleted.
+// Something reasonable would be for example to do this every day.
+//
+// When the daemon gets started it immediately deletes invalid keys, so make
+// sure you start it after calling init.
+//
+// The context parameter can be set to nil and the daemon runs forever.
+// If it is set to a context however it will listen on the context.Done
+// channel and stop once it receives a stop signal.
+// See the wiki for an example.
 func (c *SessionController) DeleteEntriesDaemon(sleep time.Duration, ctx context.Context, reportErr bool) {
 	go func() {
 		if ctx == nil {
